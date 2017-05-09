c0d39f3b80313974b37960ca0efed761a65634a1
Norbert Cyrklaff <the.last.of.us.08.08@gmail.com>
2017-05-09 14:05:05 +0200

    Refactoring.

--- Real_Device_App/app/src/main/java/com/example/cyrklafpat/real_device_app/UserLocation.java
+++ Real_Device_App/app/src/main/java/com/example/cyrklafpat/real_device_app/UserLocation.java
@@ -15,12 +15,14 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
 public class UserLocation extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
-        GoogleApiClient.ConnectionCallbacks {
+        GoogleApiClient.ConnectionCallbacks
+{
 
     private GoogleApiClient my_google_api_client;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_location);
 
@@ -33,45 +35,60 @@ public class UserLocation extends AppCompatActivity implements GoogleApiClient.O
                 .build();
     }
 
-    protected void onStart() {
+    protected void onStart()
+    {
         my_google_api_client.connect();
         super.onStart();
     }
 
-    protected void onStop() {
+    protected void onStop()
+    {
         my_google_api_client.disconnect();
         super.onStop();
     }
 
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult var1) {
-        //Do something.
-    }
+
 
     @Override
     public void onConnected(Bundle var1)
     {
         TextView text_view_handle = (TextView) findViewById(R.id.textView5);
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED)
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED)
+        {
+            // TODO: Consider calling ActivityCompat#requestPermissions
+            // to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(...)
+            // to handle the case where the user grants the permission.
+
+            Location user_LastLocation = LocationServices.FusedLocationApi.getLastLocation(my_google_api_client);
+            if (user_LastLocation != null) {
+
+                text_view_handle.setText(String.valueOf(user_LastLocation.getLatitude()));
+
+                text_view_handle.append("\n");
+                text_view_handle.append(String.valueOf(user_LastLocation.getLongitude()));
+            }
+        }
+        else
         {
             text_view_handle.setText("Error: First enable LOCATION ACCESS in settings.");
+            return;
         }
 
-        Location user_LastLocation = LocationServices.FusedLocationApi.getLastLocation(my_google_api_client);
-        if (user_LastLocation != null) {
-
-            text_view_handle.setText(String.valueOf(user_LastLocation.getLatitude()));
-
-            text_view_handle.append("\n");
-            text_view_handle.append(String.valueOf(user_LastLocation.getLongitude()));
-        }
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
+    public void onConnectionFailed(@NonNull ConnectionResult var1)
+    {
+        // We tried to connect but failed!
+    }
 
+    @Override
+    public void onConnectionSuspended(int i)
+    {
+        // We are not connected anymore!
     }
 }
