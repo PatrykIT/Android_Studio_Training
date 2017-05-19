e92592ee5f6e7fe462c18b985cabc90873e203c8
Norbert Cyrklaff <the.last.of.us.08.08@gmail.com>
2017-05-19 16:19:59 +0200

    Fighting with permissions.

--- Real_Device_App/app/src/main/java/com/example/cyrklafpat/real_device_app/UserLocation.java
+++ Real_Device_App/app/src/main/java/com/example/cyrklafpat/real_device_app/UserLocation.java
@@ -1,7 +1,10 @@
 package com.example.cyrklafpat.real_device_app;
 
+import android.Manifest;
+
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -10,8 +13,10 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.os.ResultReceiver;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spannable;
@@ -43,6 +48,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
 import java.util.ArrayList;
+import java.util.jar.*;
+
+import javax.xml.datatype.Duration;
 
 
 /** To use the Google’s Location Services, your app needs to connect to the GooglePlayServicesClient.
@@ -217,11 +225,53 @@ public class UserLocation extends AppCompatActivity implements GoogleApiClient.O
     @Override
     public void onConnected(Bundle var1)
     {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED)
+//        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED &&
+//                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED)
+//        {
+
+        /* Do not use Activity.checkSelfPermission, because it works only on API 23 and above. */
+        int has_location_permissions = ContextCompat.checkSelfPermission(UserLocation.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(has_location_permissions != PackageManager.PERMISSION_GRANTED)
         {
+            /* If User denied a permission, in the second launch, user will get a "Never ask again" option to prevent
+            application from asking this permission in the future. This case has to be handled as well.
+
+            Before calling requestPermissions, we need to check whether we should ask for permissions or show dialog like:
+            "You need to allow access to [PERMISSION] in settings."
+
+            This is done via shouldShowRequestPermissionRationale(). Note that it will return false if API is < 23 */
+            if(ActivityCompat.shouldShowRequestPermissionRationale(UserLocation.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+                ActivityCompat.requestPermissions(UserLocation.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+            }
+            else
+            {
+                DialogInterface.OnClickListener ok_listener = new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Toast.makeText(getApplicationContext(), "ok listener called", Toast.LENGTH_SHORT).show();
+                        ActivityCompat.requestPermissions(UserLocation.this,
+                                new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_ASK_PERMISSIONS);
+                    }
+                };
+
+                String message = "You need to allow access to location.";
+                new AlertDialog.Builder(UserLocation.this).setMessage(message).setPositiveButton("OK", ok_listener)
+                        .setNegativeButton("Cancel", null).create().show();
+
+                //return; //TODO: I think we should return only if user clicks 'Cancel'
+            }
+
+            //return; //TODO: Should it be here? I think if user allows permissions, we can go on.
+        }
              /* TODO: Consider calling ActivityCompat#requestPermissions
              to request the missing permissions, and then overriding
              public void onRequestPermissionsResult(...)
@@ -291,7 +341,7 @@ public class UserLocation extends AppCompatActivity implements GoogleApiClient.O
                     text_view_handle.append("Altitude: " + String.valueOf(user_last_location.getAltitude()));
                 }
             }
-        }
+        //}
 
         if(Geocoder.isPresent() == false)
         {
@@ -371,6 +421,10 @@ public class UserLocation extends AppCompatActivity implements GoogleApiClient.O
     /** Used to persist application state about whether geofences were added. */
     private SharedPreferences shared_preferences;
 
+    /** Application specific request code to match with a result reported to
+     * {@link ActivityCompat.OnRequestPermissionsResultCallback#onRequestPermissionsResult */
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
 
 
     @Override
@@ -426,11 +480,64 @@ public class UserLocation extends AppCompatActivity implements GoogleApiClient.O
             Toast.makeText(this, "Not connected Google API!", Toast.LENGTH_SHORT).show();
             return;
         }
+        /* Do not use Activity.checkSelfPermission, because it works only on API 23 and above. */
+        int has_location_permissions = ContextCompat.checkSelfPermission(UserLocation.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
 
-        try
+        if(has_location_permissions != PackageManager.PERMISSION_GRANTED)
         {
-            LocationServices.GeofencingApi.addGeofences(my_google_api_client, getGeofencingRequest(),
-                    getGeofencePendingIntent()).setResultCallback(this);
+            /* If User denied a permission, in the second launch, user will get a "Never ask again" option to prevent
+            application from asking this permission in the future. This case has to be handled as well.
+
+            Before calling requestPermissions, we need to check whether we should ask for permissions or show dialog like:
+            "You need to allow access to [PERMISSION] in settings."
+
+            This is done via shouldShowRequestPermissionRationale(). */
+            if(ActivityCompat.shouldShowRequestPermissionRationale(UserLocation.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) == false)
+            {
+                DialogInterface.OnClickListener ok_listener = new DialogInterface.OnClickListener()
+                {
+                 @Override
+                    public void onClick(DialogInterface dialog, int which)
+                 {
+                     ActivityCompat.requestPermissions(UserLocation.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                             REQUEST_CODE_ASK_PERMISSIONS);
+                 }
+                };
+
+                String message = "You need to allow access to location.";
+                new AlertDialog.Builder(UserLocation.this).setMessage(message).setPositiveButton("OK", ok_listener)
+                        .setNegativeButton("Cancel", null).create().show();
+
+                return; //TODO: I think we should return only if user clicks 'Cancel'
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(UserLocation.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+            }
+
+            return; //TODO: Should it be here? I think if user allows permissions, we can go on.
+        }
+        LocationServices.GeofencingApi.addGeofences(my_google_api_client, getGeofencingRequest(),
+                getGeofencePendingIntent()).setResultCallback(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(getApplicationContext(), "Permission granted!!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Something went wrong.", Toast.LENGTH_SHORT).show();
+                }
         }
     }
 }
